9b93d24b0f9b3ef4c591484297cbbf7bd946b619
Aman Yadav <yadavaman150902@gmail.com>
2024-08-20 01:26:38 +0530

    Conversion

---
+++ Chai-aur-JavaScript/01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";   // treat all JS code as newer version
+
+// alert("Hello")  // we are using node js not browser so it gives error
+
+console.log( 3
+   + 3 ); // code readbility should be high.
+
+// H.W--Documentation of javascript----ECMAScript--tc39.es and mdn
+
+console.log("Aman");
+
+let name = "Aman"
+let age = 22
+let isLoggedIn = true
+let State;         // undefined hai
+let state = null   // undefined ni hai khali hai
+
+/*
+Number--->range of memory address 2^53
+bigint
+string--->use in doubles qoutes
+boolean--->true/false
+null-->stadnd alone value
+undefined-->
+symbol--->unique
+
+object--->
+*/
+
+
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ Chai-aur-JavaScript/01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+let score1 = "33"
+let score2 = "33abc"
+let score3 = null
+let score4 = undefined
+let score5 = true
+
+// console.log(typeof score1);
+// console.log(typeof score2);
+// console.log(typeof score3);
+// console.log(typeof score4);
+// console.log(typeof score5);
+
+let convertInNumber1 = Number(score1)
+let convertInNumber2 = Number(score2)
+let convertInNumber3= Number(score3)
+let convertInNumber4 = Number(score4)
+let convertInNumber5 = Number(score5)
+// console.log(convertInNumber1);
+// console.log(convertInNumber2);
+// console.log(convertInNumber3);
+// console.log(convertInNumber4);
+// console.log(convertInNumber5);
+
+
+
+
+
+
+/* 
+Type Conversion in Javascript
+1-  "33"--> 33
+2-  "33abc"--> NaN
+3-  true-->1; false-->0
+*/
+
+let loggedIn = "aman"
+
+let booltoLogged =  Boolean(loggedIn)
+// console.log(booltoLogged);
+
+// 1-> true; 0--> false
+// ""--> false
+// "aman"--> true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
