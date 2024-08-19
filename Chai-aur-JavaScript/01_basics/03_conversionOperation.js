6abb8f88c98f5745b939a5f08fa8c44f73e80512
Aman Yadav <yadavaman150902@gmail.com>
2024-08-20 01:44:51 +0530

    update

--- Chai-aur-JavaScript/01_basics/03_conversionOperation.js
+++ Chai-aur-JavaScript/01_basics/03_conversionOperation.js
@@ -46,6 +46,45 @@ let booltoLogged =  Boolean(loggedIn)
 let someNumber = 22
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ********************* Operations *************************//
+
+let value = 3
+
+let negvalue = -value
+// console.log(negvalue);
+
+
+let str1 = "Hello"
+let str2 = " Aman"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// whoseover first conversion are become in that datatype
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 3 // do not use in these tricky in production code
+
+
+let counter = 100
+counter++
+console.log(counter);
+
 
