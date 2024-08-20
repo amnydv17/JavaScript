3309e369ebf38d0818f8801b671418e1ae470bed
Aman Yadav <yadavaman150902@gmail.com>
2024-08-21 04:48:49 +0530

    commit file

---
+++ Chai-aur-JavaScript/01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+
+// &&&&&&&&&&&&&&   NUMBER      **********************
+
+const score = 400
+
+const balance = new Number(199);
+// console.log(score);
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNum = 12333.832165
+// console.log(otherNum.toPrecision(3));
+
+
+const hundred = 100000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+
+
+// $$$$$$$$$$$$$$$$$$$     MATHS      ###################
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,3,5,7,9));
+// console.log(Math.max(4,3,5,7,9));
+console.log(Math.random());   // 0-1 k bich ki value
+console.log((Math.random()*10) + 1);   
+
+// Trick
+const min = 11
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
